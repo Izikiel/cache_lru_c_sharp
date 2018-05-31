c791411138c9c3018594e29a959df9d93b4f197f
Izikiel <ezequiel.gambaccini@gmail.com>
2018-05-30 22:10:58 -0300

    Commit inicial

---
+++ src/CacheLRU.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CacheLRU
+{
+    public class CacheLRU<Tkey, Tvalue> : IDictionary<Tkey, Tvalue>
+    {
+        readonly int size;
+        int count;
+        LinkedList<KeyValuePair<Tkey, Tvalue>> lru_order;
+        Dictionary<Tkey, LinkedListNode<KeyValuePair<Tkey, Tvalue>>> dict;
+
+        public CacheLRU(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException("size cannot be 0 or less");
+            }
+
+            this.size = size;
+            count = 0;
+            lru_order = new LinkedList<KeyValuePair<Tkey, Tvalue>>();
+            dict = new Dictionary<Tkey, LinkedListNode<KeyValuePair<Tkey, Tvalue>>>();
+        }
+
+        private void RemoveLRU()
+        {
+            if (Lru_order.Count == 0)
+            {
+                return;
+            }
+
+            Dict.Remove(Lru_order.First.Value.Key);
+            Lru_order.RemoveFirst();
+            count--;
+        }
+
+        public Tvalue this[Tkey key]
+        {
+            get => Dict[key].Value.Value;
+            set => Update(key, value);
+        }
+
+        private void Update(Tkey key, Tvalue value) {
+            Remove(key);
+            Add(key, value);
+        }
+
+        public ICollection<Tkey> Keys => this.Dict.Keys;
+
+        public ICollection<Tvalue> Values => (ICollection<Tvalue>)from node in Dict.Values
+                                                                  select node.Value.Value;
+
+        public int Count => this.Dict.Count;
+
+        public bool IsReadOnly => false;
+
+        private Dictionary<Tkey, LinkedListNode<KeyValuePair<Tkey, Tvalue>>> Dict { get => dict; }
+        public LinkedList<KeyValuePair<Tkey, Tvalue>> Lru_order { get => lru_order; }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            if (Dict.ContainsKey(key))
+            {
+                throw new ArgumentException("Element already in cache");
+            }
+            if (count == size)
+            {
+                RemoveLRU();
+            }
+            var pair = new KeyValuePair<Tkey, Tvalue>(key, value);
+            Add(pair);
+
+        }
+
+        public void Add(KeyValuePair<Tkey, Tvalue> item)
+        {
+            if (Dict.ContainsKey(item.Key))
+            {
+                throw new ArgumentException("Element already in cache");
+            }
+
+            if (count == size)
+            {
+                RemoveLRU();
+            }
+
+            var node = new LinkedListNode<KeyValuePair<Tkey, Tvalue>>(item);
+            Dict.Add(item.Key, node);
+            Lru_order.AddLast(node);
+            count++;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            Lru_order.Clear();
+            Dict.Clear();
+        }
+
+        public bool Contains(KeyValuePair<Tkey, Tvalue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(Tkey key)
+        {
+            return Dict.ContainsKey(key);
+        }
+
+        public void CopyTo(KeyValuePair<Tkey, Tvalue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<Tkey, Tvalue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Tkey key, out Tvalue value){
+            LinkedListNode<KeyValuePair<Tkey, Tvalue>> node;
+            if (Dict.Remove(key, out node))
+            {
+                Lru_order.Remove(node);
+                count--;
+                value = node.Value.Value;
+                return true;
+            }
+            value = default(Tvalue);
+            return false;
+        }
+        public bool Remove(Tkey key)
+        {
+            LinkedListNode<KeyValuePair<Tkey, Tvalue>> node;
+            if (Dict.Remove(key, out node))
+            {
+                Lru_order.Remove(node);
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remove(KeyValuePair<Tkey, Tvalue> item) => Remove(item.Key);
+
+        public bool TryGetValue(Tkey key, out Tvalue value)
+        {
+            LinkedListNode<KeyValuePair<Tkey, Tvalue>> node;
+
+            if (Dict.TryGetValue(key, out node))
+            {
+                value = node.Value.Value;
+                return true;
+            }
+            value = default(Tvalue);
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CacheLRU;
+
+namespace CacheLRU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cache = new CacheLRU<int, int>(10);
+            for (int i = 0; i < 10; i++)
+            {
+                cache.Add(i, i*i);
+            }
+
+            Console.WriteLine($"0 in cache? {cache.ContainsKey(0)}");
+
+            cache.Add(11, 11*11);
+
+            Console.WriteLine($"0 in cache? {cache.ContainsKey(0)}");
+            int res = 13;
+
+            cache.Remove(3, out res);
+
+            Console.WriteLine($"Removed 3 and got {res}");
+
+            cache.Remove(3, out res);
+
+            Console.WriteLine($"Tried to remove 3 again and got {res}");
+
+        }
+    }
+}
